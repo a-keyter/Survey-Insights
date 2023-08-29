6aeb2e2e492eaa850741df762716c4de76cfdd1b
alexkeyter <keyteraj@cardiff.ac.uk>
2023-08-29 20:11:10 +0100

    set openaikey

--- home.py
+++ home.py
@@ -6,10 +6,15 @@ import io
 
 from docx import Document
 
+#Import LLM - Langchain
+from langchain.llms import OpenAI
+
 # TEST VERSION #
 test = False
 if test == True:
-    from apikeys import openaikey  
+    from apikeys import openaikey
+    os.environ['OPENAI_API_KEY'] = openaikey
+    llm = OpenAI(temperature=0.4)
 
 # CLOUD VERSION #
 # openaikey = st.secrets['OPENAI_API_KEY']
@@ -17,16 +22,12 @@ if test == True:
 # AI STUFF BELOW
 # ____________________
 
-#Import LLM - Langchain
-from langchain.llms import OpenAI
 
 #Langchain Features
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
-os.environ['OPENAI_API_KEY'] = openaikey
 
-llm = OpenAI(temperature=0.4)
 
 response_summary_template = PromptTemplate(
     input_variables = ["survey_question", "responses"],
